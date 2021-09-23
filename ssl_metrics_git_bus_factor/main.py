a6fe274152cab7a9e7b78ae91f423ba9661efb18
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2021-09-23 18:18:10 -0500

    Delete fill day function

--- ssl_metrics_git_bus_factor/main.py
+++ ssl_metrics_git_bus_factor/main.py
@@ -1,5 +1,28 @@
 from json import dump, load
 from typing import Any
+from argparse import ArgumentParser, Namespace
+
+
+def get_argparse() -> Namespace:
+    parser: ArgumentParser = ArgumentParser(
+        prog="SSL Metrics - Bus Factor Calculator",
+        usage="Takes a JSON file of commits as an input and returns the bus factor of commits for each day that the project exists.",
+    )
+    parser.add_argument(
+        "-i",
+        "--input",
+        help="The input JSON file that is to be used for calculating the bus factor",
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        help="The bus factor output JSON",
+        type=str,
+        required=True,
+    )
+    return parser.parse_args()
 
 
 def loadJSON(filename: str) -> list:
@@ -47,28 +70,6 @@ def buildBusFactor(commits: list) -> dict:
     return data
 
 
-def fillMissingDays(data: dict) -> dict:
-    days: list = list(data.keys())
-
-    index: int
-    for index in range(len(days)):
-        try:
-            difference: int = days[index + 1] - days[index]
-            if difference == 1:
-                pass
-            else:
-                differenceIndex: int
-                for differenceIndex in range(difference):
-                    if differenceIndex == 0:
-                        pass
-                    else:
-                        data[index + differenceIndex] = []
-        except IndexError:
-            pass
-
-    return data
-
-
 def dumpJSON(json: Any, filename: str) -> None:
     with open(file=filename, mode="w") as file:
         dump(json, file)
@@ -76,11 +77,16 @@ def dumpJSON(json: Any, filename: str) -> None:
 
 
 def main() -> None:
-    data: list = loadJSON("commits.json")
+    args: Namespace = get_argparse()
+    data: list = loadJSON(filename=args.input)
     bf: dict = buildBusFactor(data)
-    bf: dict = fillMissingDays(bf)
-    dumpJSON(bf, "temp.json")
+    dumpJSON(bf, args.output)
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":  # maxDataRecords: int = 0
+    # for item in data:
+    #     maxlen(item))
+
+    # print(maxDataRecords)a
+
     main()
