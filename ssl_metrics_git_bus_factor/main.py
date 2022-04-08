5d038c5d48fab41477ae59aea7c5bdc5977c3ff8
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-04-08 12:18:01 -0500

    Update main.py

--- ssl_metrics_git_bus_factor/main.py
+++ ssl_metrics_git_bus_factor/main.py
@@ -1,11 +1,12 @@
-from argparse import ArgumentParser, Namespace
+from argparse import Namespace
 
 import pandas
 from pandas import DataFrame, Series
 
+from ssl_metrics_git_bus_factor.args import mainArgs
 
 def buildBusFactor(df: DataFrame) -> DataFrame:
-    daysSince0: Series = df["days_since_0"].unique()
+    daysSince0: Series = df["author_days_since_0"].unique()
 
     data: list = []
 
@@ -13,26 +14,21 @@ def buildBusFactor(df: DataFrame) -> DataFrame:
     for day in range(daysSince0.max() + 1):
         temp: dict = {}
 
-        busFactor: int = len(df[df["days_since_0"] == day]["author_email"].unique())
+        busFactor: int = len(df[df["author_days_since_0"] == day]["author_email"].unique())
 
         temp["days_since_0"] = day
         temp["bus_factor"] = busFactor
 
         data.append(temp)
+
     return DataFrame(data)
 
 
 def main() -> None:
-    args: Namespace = get_argparse()
+    args: Namespace = mainArgs()
 
-    if args.input[-5::] != ".json":
-        print("Invalid input file type. Input file must be JSON")
-        quit(1)
-
-    dfIn: DataFrame = pandas.read_json(args.input)
-    dfOut: DataFrame = buildBusFactor(df=dfIn)
-
-    dfOut.to_json(args.output)
+    df: DataFrame = pandas.read_json(args.input)
+    buildBusFactor(df).to_json(args.output)
 
 
 if __name__ == "__main__":
