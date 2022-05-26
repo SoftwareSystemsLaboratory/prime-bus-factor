7a87de00e9cc742a7e63c43252863aa87b8f4799
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-05-26 09:42:14 -0500

    Implemented Bus Factor

--- clime_bus_factor/bus_factor.py
+++ clime_bus_factor/bus_factor.py
@@ -1,12 +1,15 @@
-from pandas import DataFrame
-from clime_bus_factor.args import busFactorArgs
 from argparse import Namespace
-import pandas
 
-def get_devs(df: DataFrame, *, bin: int, alpha=0.0) -> DataFrame:
-    day_key = "day_since_0"
+import pandas
+from pandas import DataFrame
+
+from clime_bus_factor.args import busFactorArgs
+
+
+def get_devs(df: DataFrame, *, bin: int, alpha: float = 0.0) -> DataFrame:
+    day_key = "author_days_since_0"
     lastday = df[day_key].max() + bin
-    bins = list(range(0,lastday,bin))
+    bins = list(range(0, lastday, bin))
 
     df["commitBin"] = pandas.cut(df[day_key], bins=bins, include_lowest=True)
     bins = df["commitBin"].unique().tolist()
@@ -14,17 +17,19 @@ def get_devs(df: DataFrame, *, bin: int, alpha=0.0) -> DataFrame:
     data = []
     for bin in bins:
 
-        item = {"days_since_0" : int(bin.left) if bin.left > 0 else 0}
+        item = {"days_since_0": int(bin.left) if bin.left > 0 else 0}
 
         if alpha:
             temp = df[df["commitBin"] == bin]
-            abs_list = lambda l : [abs(item) for item in l]
+            abs_list = lambda l: [abs(item) for item in l]
             significance = alpha * sum(abs_list(temp["delta_loc"].tolist()))
 
             bf = 0
             authors = set(temp["author_email"].tolist())
             for author in authors:
-                author_dloc = sum(abs_list(temp[temp["author_email"] == author]["delta_loc"].tolist()))
+                author_dloc = sum(
+                    abs_list(temp[temp["author_email"] == author]["delta_loc"].tolist())
+                )
                 if author_dloc > significance:
                     bf += 1
 
@@ -32,7 +37,7 @@ def get_devs(df: DataFrame, *, bin: int, alpha=0.0) -> DataFrame:
 
             item["bus_factor"] = bf
         else:
-            item["devs"] = len( df[df["commitBin"] == bin]["author_email"].unique() )
+            item["devs"] = len(df[df["commitBin"] == bin]["author_email"].unique())
 
         data.append(item)
 
@@ -41,15 +46,19 @@ def get_devs(df: DataFrame, *, bin: int, alpha=0.0) -> DataFrame:
 
 def main() -> None:
 
-    args: Namespace = main_args()
+    args: Namespace = busFactorArgs()
 
-    if not args.alpha:
-        print(f'bus_factor calculation requires alpha value')
-        quit()
+    if args.alpha > 1:
+        print("Invalid alpha value. Must be alpha =< 1 and alpha >= 0")
+        quit(1)
+    if args.alpha < 0:
+        print("Invalid alpha value. Must be alpha =< 1 and alpha >= 0")
+        quit(2)
 
-    df = pandas.read_json(args.input) #.T
+    df = pandas.read_json(args.input)  # .T
     bf = get_devs(df, bin=args.bin, alpha=args.alpha)
     bf.to_json(args.output, indent=4)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
