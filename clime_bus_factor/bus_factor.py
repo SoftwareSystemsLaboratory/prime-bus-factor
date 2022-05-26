dc99a8c8b826d521acbf9c8cf1613fd6173280c8
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-05-26 10:33:54 -0500

    Redue module to only use the bus_factor.py file

--- clime_bus_factor/bus_factor.py
+++ clime_bus_factor/bus_factor.py
@@ -1,12 +1,11 @@
 from argparse import Namespace
 
 import pandas
-from pandas import DataFrame
+from pandas import Categorical, DataFrame, Interval, Series
 
-from clime_bus_factor.args import busFactorArgs
+from clime_bus_factor.args import developerCountArgs
 
-
-def get_devs(df: DataFrame, *, bin: int, alpha: float = 0.0) -> DataFrame:
+def buildBusFactor(df: DataFrame, *, bin: int, alpha: float = 0.0) -> DataFrame:
     day_key = "author_days_since_0"
     lastday = df[day_key].max() + bin
     bins = list(range(0, lastday, bin))
@@ -43,21 +42,24 @@ def get_devs(df: DataFrame, *, bin: int, alpha: float = 0.0) -> DataFrame:
 
     return DataFrame(data)
 
-
 def main() -> None:
+    args: Namespace = developerCountArgs()
 
-    args: Namespace = busFactorArgs()
-
+    if args.bin < 1:
+        print(f"Bin arguement must be an integer greater than 0: {args.bin}")
+        quit(1)
     if args.alpha > 1:
         print("Invalid alpha value. Must be alpha =< 1 and alpha >= 0")
-        quit(1)
+        quit(2)
     if args.alpha < 0:
         print("Invalid alpha value. Must be alpha =< 1 and alpha >= 0")
-        quit(2)
+        quit(3)
 
     df: DataFrame = pandas.read_json(args.input).T
-    bf: DataFrame = get_devs(df, bin=args.bin, alpha=args.alpha)
-    bf.to_json(args.output, indent=4)
+    bf: DataFrame = buildBusFactor(df, bin=args.bin, alpha=args.alpha)
+    cd: DataFrame = buildBusFactor(df, bin=args.bin, alpha=0)
+
+    pandas.concat([bf, cd]).to_json(args.output, indent=4)
 
 
 if __name__ == "__main__":
